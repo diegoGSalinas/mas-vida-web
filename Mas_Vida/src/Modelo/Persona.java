ca3d116e3a4ba81ec0c9c00382e964601e47407f
Diego GS <dagonzales.salinas@gmail.com>
2025-06-04 12:44:21 -0500

    dto clase persona

--- Mas_Vida/src/Modelo/Persona.java
+++ Mas_Vida/src/Modelo/Persona.java
@@ -9,5 +9,51 @@ package Modelo;
  * @author Diego
  */
 public class Persona {
-    
+
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String direccion;
+    private String correo;
+
+    // Getters y Setters
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
